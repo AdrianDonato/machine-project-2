7cba459cd96ca521ebe144361eede1bc7ae72105
AdrianDonato <67679435+AdrianDonato@users.noreply.github.com>
2020-08-31 13:34:30 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const session = require("express-session")
+const bodyparser = require("body-parser")
+const cookieparser = require("cookie-parser")
+
+const app = express()
+const urlencoder = bodyparser.urlencoded({
+    extended:false
+})
+
+app.use(session({
+    secret:"very secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie:{
+        maxAge: 1000*60*60
+    }
+}))
+
+app.listen(3000, function(){
+    console.log("listening to port 3000")
+})

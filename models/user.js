57d9636ac31f74e07957916fffd06b35f66c3ad1
AdrianDonato <67679435+AdrianDonato@users.noreply.github.com>
2020-09-07 18:52:20 +0800

    Update user.js

--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ let User = mongoose.model("user", {
     username: String,
     password: String,
     email: String,
+    role: String,
     firstname: String,
     lastname: String,
     reviews: [

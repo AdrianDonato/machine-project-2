03ee4f25f85cb8e9d2d035c9dc4568a9ede37685
AdrianDonato <67679435+AdrianDonato@users.noreply.github.com>
2020-09-08 14:12:40 +0800

    Update website.js

--- models/website.js
+++ models/website.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 let Website = mongoose.model("website", {
     websitename: String,
     category: String,
+    websitedesc: String,
     avg_designscore: Number,
     avg_usabilityscore: Number,
     avg_contentscore: Number,
